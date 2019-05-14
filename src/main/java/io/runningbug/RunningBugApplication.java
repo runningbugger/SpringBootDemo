d42e65327acddbe9b1b6a69cdd2c2680c859c8b2
runningbugger <runningbugger@gmail.com>
2019-05-14 22:00:51 +0800

    init

---
+++ src/main/java/io/runningbug/RunningBugApplication.java
@@ -0,0 +1,13 @@
+package io.runningbug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningBugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunningBugApplication.class, args);
+	}
+
+}
